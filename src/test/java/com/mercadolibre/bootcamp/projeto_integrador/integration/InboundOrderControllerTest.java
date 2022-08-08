3b54de967da2d397ee03e7d8e18b0979c1ab19f1
pealmeida-meli <pedro.almeida@mercadolivre.com>
2022-08-08 11:40:44 -0300

    chore: finish first integration tests

--- src/test/java/com/mercadolibre/bootcamp/projeto_integrador/integration/InboundOrderControllerTest.java
+++ src/test/java/com/mercadolibre/bootcamp/projeto_integrador/integration/InboundOrderControllerTest.java
@@ -12,22 +12,20 @@ import com.mercadolibre.bootcamp.projeto_integrador.repository.IManagerRepositor
 import com.mercadolibre.bootcamp.projeto_integrador.repository.IProductRepository;
 import com.mercadolibre.bootcamp.projeto_integrador.repository.ISectionRepository;
 import com.mercadolibre.bootcamp.projeto_integrador.repository.IWarehouseRepository;
-import org.jeasy.random.EasyRandom;
-import org.jeasy.random.EasyRandomParameters;
-import org.jeasy.random.FieldPredicates;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -41,9 +39,6 @@ public class InboundOrderControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private ApplicationContext context;
 
     @Autowired
     private ISectionRepository sectionRepository;
@@ -60,54 +55,55 @@ public class InboundOrderControllerTest {
     @Autowired
     private DataSource dataSource;
 
-    private static final ObjectMapper objectMapper;
+    private final ObjectMapper objectMapper;
 
-    static {
+    public InboundOrderControllerTest() {
         objectMapper = new ObjectMapper();
         objectMapper.findAndRegisterModules();
     }
 
-    static String asJsonString(final Object obj) throws JsonProcessingException {
-        return objectMapper.writeValueAsString(obj);
-    }
-
     @BeforeEach
-    void cleanAllRepositories() throws SQLException {
+    void cleanDatabase() throws SQLException {
         try (
-            var connection = dataSource.getConnection();
-            var statement = connection.createStatement()
+            Connection connection = dataSource.getConnection();
+            Statement statement = connection.createStatement()
         ) {
             statement.execute("SET REFERENTIAL_INTEGRITY FALSE");
-            context.getBeansOfType(JpaRepository.class).values().forEach(CrudRepository::deleteAll);
+            ResultSet records = statement.executeQuery("SHOW TABLES");
+
+            while (records.next()) {
+                Statement truncateStatement = connection.createStatement();
+                truncateStatement.executeUpdate("TRUNCATE TABLE " + records.getString(1));
+                truncateStatement.close();
+            }
+
             statement.execute("SET REFERENTIAL_INTEGRITY TRUE");
         }
     }
 
     @Test
     void createInboundOrder_returnsOk_whenIsGivenAValidInput() throws Exception {
-        EasyRandomParameters parameters = new EasyRandomParameters();
-        parameters.excludeField(FieldPredicates.named("^.+(Code|Id|Number)$"));
-
-        EasyRandom generator = new EasyRandom(parameters);
-        var warehouse = generator.nextObject(Warehouse.class);
-        var manager = generator.nextObject(Manager.class);
-        var section = generator.nextObject(Section.class);
-        section.setWarehouse(warehouse);
-        section.setManager(manager);
-        var product = generator.nextObject(Product.class);
+        Warehouse warehouse = getWarehouse();
+        Manager manager = getManager();
+        Section section = getSection(warehouse, manager);
+        Product product = getProduct();
 
         warehouseRepository.save(warehouse);
         managerRepository.save(manager);
         sectionRepository.save(section);
         productRepository.save(product);
 
-        var batchRequest = generator.nextObject(BatchRequestDto.class);
+        BatchRequestDto batchRequest = new BatchRequestDto();
         batchRequest.setProductId(product.getProductId());
+        batchRequest.setProductPrice(new BigDecimal("100.99"));
+        batchRequest.setCurrentTemperature(10.0f);
+        batchRequest.setMinimumTemperature(10.0f);
         batchRequest.setDueDate(LocalDate.now().plusWeeks(1));
-        batchRequest.setManufacturingDate(LocalDate.now());
         batchRequest.setManufacturingTime(LocalDateTime.now());
+        batchRequest.setManufacturingDate(LocalDate.now());
+        batchRequest.setInitialQuantity(10);
 
-        var requestDto = new InboundOrderRequestDto();
+        InboundOrderRequestDto requestDto = new InboundOrderRequestDto();
         requestDto.setBatchStock(List.of(batchRequest));
         requestDto.setSectionCode(section.getSectionCode());
 
@@ -115,5 +111,77 @@ public class InboundOrderControllerTest {
                 .content(asJsonString(requestDto))
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated());
+    }
+
+    @Test
+    void createInboundOrder_returnsError_whenIsGivenAnInvalidInput() throws Exception {
+        Warehouse warehouse = getWarehouse();
+        Manager manager = getManager();
+        Section section = getSection(warehouse, manager);
+        Product product = getProduct();
+
+        warehouseRepository.save(warehouse);
+        managerRepository.save(manager);
+        sectionRepository.save(section);
+        productRepository.save(product);
+
+        System.out.println(manager.getManagerId());
+
+        BatchRequestDto batchRequest = new BatchRequestDto();
+        batchRequest.setProductId(product.getProductId());
+
+        // Valores inválidos.
+        batchRequest.setProductPrice(new BigDecimal("-100.99"));
+        batchRequest.setCurrentTemperature(-1);
+        batchRequest.setMinimumTemperature(-1);
+        batchRequest.setDueDate(LocalDate.now().minusWeeks(1));
+        batchRequest.setManufacturingTime(LocalDateTime.now().plusDays(1));
+        batchRequest.setManufacturingDate(LocalDate.now().plusDays(1));
+        batchRequest.setInitialQuantity(-1);
+
+        InboundOrderRequestDto requestDto = new InboundOrderRequestDto();
+        requestDto.setBatchStock(List.of(batchRequest));
+        requestDto.setSectionCode(section.getSectionCode());
+
+        mockMvc.perform(post("/api/v1/fresh-products/inboundorder")
+                        .content(asJsonString(requestDto))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is4xxClientError());
+    }
+
+    private Warehouse getWarehouse() {
+        Warehouse warehouse = new Warehouse();
+        warehouse.setLocation("New York");
+        return warehouse;
+    }
+
+    private Product getProduct() {
+        Product product = new Product();
+        product.setProductName("Apple");
+        product.setBrand("Nature");
+        product.setCategory("Fruit");
+        return product;
+    }
+
+    private Section getSection(Warehouse warehouse, Manager manager) {
+        Section section = new Section();
+        section.setCurrentBatches(1);
+        section.setCategory(Section.Category.FRESH);
+        section.setWarehouse(warehouse);
+        section.setManager(manager);
+        section.setMaxBatches(10);
+        return section;
+    }
+
+    private Manager getManager() {
+        Manager manager = new Manager();
+        manager.setName("John Doe");
+        manager.setUsername("john");
+        manager.setEmail("john@example.com");
+        return manager;
+    }
+
+    private String asJsonString(final Object obj) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(obj);
     }
 }
