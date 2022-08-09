4e4e78afd790cfb6197ef115baa2f05fd329ca17
˜Thiago <thiago.sordi@mercadolivre.com>
2022-08-09 10:20:15 -0300

    feature: validation of purchase's dto

--- src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/ProductDto.java
+++ src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/ProductDto.java
@@ -1,6 +1,16 @@
 package com.mercadolibre.bootcamp.projeto_integrador.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 public class ProductDto {
+
+    @NotNull(message = "O id do produto não pode estar vazio")
+    @Positive(message = "O id do produto deve ser um número positivo")
     private long productId;
+
+    @NotNull(message = "A quantidade do produto deve ser informada")
+    @Min(value = 0, message = "A quantidade do produto deve ser maior ou igual a 0")
     private int quantity;
 }
--- src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/PurchaseOrderRequestDto.java
+++ src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/PurchaseOrderRequestDto.java
@@ -1,11 +1,27 @@
 package com.mercadolibre.bootcamp.projeto_integrador.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
 import java.time.LocalDate;
 import java.util.List;
 
 public class PurchaseOrderRequestDto {
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    @NotNull(message = "A data da inserção no carrinho deve ser informada")
+    @PastOrPresent(message = "A data da inserção no carrinho deve ser menor ou igual a data atual")
     private LocalDate date;
+
+    @NotNull(message = "O id do cliente não pode estar vazio")
+    @Positive(message = "O id do cliente deve ser um número positivo")
     private long buyerId;
+
     private boolean orderStatus;
+
+    @NotEmpty(message = "A lista de produtos é obrigatória")
     private List<ProductDto> products;
 }
