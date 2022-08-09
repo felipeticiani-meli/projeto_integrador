687cdacea4767bafea4b0586735b5bb240c2e2bd
brunavottri <bruna.vottri@mercadolivre.com>
2022-08-09 17:38:22 -0300

    chore: change orderStatus name to isFinished

--- src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/ProductDto.java
+++ src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/ProductDto.java
@@ -1,5 +1,10 @@
 package com.mercadolibre.bootcamp.projeto_integrador.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class ProductDto {
     private long productId;
     private int quantity;
--- src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/PurchaseOrderRequestDto.java
+++ src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/PurchaseOrderRequestDto.java
@@ -1,11 +1,16 @@
 package com.mercadolibre.bootcamp.projeto_integrador.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
 public class PurchaseOrderRequestDto {
     private LocalDate date;
     private long buyerId;
-    private boolean orderStatus;
+    private boolean isFinished;
     private List<ProductDto> products;
 }
