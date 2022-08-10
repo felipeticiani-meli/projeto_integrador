36001d51e3f0dafe1c10bc39d4c5f7d4aa279b4f
Bruna Vottri <bruna.vottri@mercadolivre.com>
2022-08-10 15:39:56 -0300

    Update src/main/java/com/mercadolibre/bootcamp/projeto_integrador/model/Buyer.java
    Co-authored-by: Pedro Thomé <pedro.almeida@mercadolivre.com>

--- src/main/java/com/mercadolibre/bootcamp/projeto_integrador/model/Buyer.java
+++ src/main/java/com/mercadolibre/bootcamp/projeto_integrador/model/Buyer.java
@@ -17,6 +17,6 @@ public class Buyer {
     @Column(length = 45)
     private String username;
 
-    @OneToMany(mappedBy = "buyerId", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "buyer", cascade = CascadeType.ALL)
     private List<PurchaseOrder> purchaseOrders;
 }
