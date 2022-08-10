9636b45f2648e46218fe3fdb2845c4f0b403fa7b
Thiago Sordi <thiago.sordi@mercadolivre.com>
2022-08-10 17:14:50 -0300

    US2: Purchase order validation and exception (#33)
    * feature: validation of purchase's dto
    
    * feature: validations and exceptions of purchaseorder
    
    Co-authored-by: Pedro Thomé <pedro.almeida@mercadolivre.com>

--- src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/ProductDto.java
+++ src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/ProductDto.java
@@ -1,11 +1,21 @@
 package com.mercadolibre.bootcamp.projeto_integrador.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class ProductDto {
+
+    @NotNull(message = "O id do produto não pode estar vazio")
+    @Positive(message = "O id do produto deve ser um número positivo")
     private long productId;
+
+    @NotNull(message = "A quantidade do produto deve ser informada")
+    @Min(value = 0, message = "A quantidade do produto deve ser maior ou igual a 0")
     private int quantity;
 }
--- src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/PurchaseOrderRequestDto.java
+++ src/main/java/com/mercadolibre/bootcamp/projeto_integrador/dto/PurchaseOrderRequestDto.java
@@ -1,6 +1,10 @@
 package com.mercadolibre.bootcamp.projeto_integrador.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
 import com.mercadolibre.bootcamp.projeto_integrador.model.Buyer;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +15,20 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 public class PurchaseOrderRequestDto {
+
     @JsonFormat(pattern = "dd-MM-yyyy")
+    @NotNull(message = "A data da inserção no carrinho deve ser informada")
+    @PastOrPresent(message = "A data da inserção no carrinho deve ser menor ou igual a data atual")
     private LocalDate date;
+
+    @NotNull(message = "O id do cliente não pode estar vazio")
+    @Positive(message = "O id do cliente deve ser um número positivo")
     private long buyerId;
+
+    @NotNull(message = "O status da compra não pode estar vazio")
+    @Pattern(regexp = "^(Closed|Opened)$", message = "Status só pode ser Opened ou Closed")
     private String orderStatus;
-    private List<ProductDto> products;
+
+    @NotEmpty(message = "A lista de produtos é obrigatória")
+    private List<@Valid ProductDto> products;
 }
