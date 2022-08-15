352c1efb1cb3396a279068062d86b978e931d14a
Bruna Vottri <bruna.vottri@mercadolivre.com>
2022-08-15 09:23:02 -0300

    delete not used commented code

--- src/test/java/com/mercadolibre/bootcamp/projeto_integrador/util/BatchPurchaseOrderGenerator.java
+++ src/test/java/com/mercadolibre/bootcamp/projeto_integrador/util/BatchPurchaseOrderGenerator.java
@@ -16,26 +16,4 @@ public class BatchPurchaseOrderGenerator {
                 .quantity(1)
                 .build();
     }
-
-//    public static BatchPurchaseOrder newBatchPurchaseOrder(PurchaseOrder purchaseOrder, Batch batch) {
-//        return BatchPurchaseOrder.builder()
-//                .batchPurchaseId(1l)
-//                .purchaseOrder(purchaseOrder)
-//                .batch(batch)
-//                .unitPrice(batch.getProductPrice())
-//                .quantity(batch.getCurrentQuantity())
-//                .build();
-//    }
-
-//    public static List<BatchPurchaseOrder> newBatchPurchaseOrderList(PurchaseOrder purchaseOrder, Batch batch) {
-//        List<BatchPurchaseOrder> batchPurchaseOrderList = new ArrayList<>();
-//        batchPurchaseOrderList.add(BatchPurchaseOrder.builder()
-//                .batchPurchaseId(1)
-//                .purchaseOrder(purchaseOrder)
-//                .batch(batch)
-//                .unitPrice(batch.getProductPrice())
-//                .quantity(batch.getCurrentQuantity())
-//                .build());
-//        return batchPurchaseOrderList;
-//    }
 }
